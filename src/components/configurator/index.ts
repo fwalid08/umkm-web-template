b6410446d14ce72aacc24487cdc98e721eb63353
Fahrul W Djuhaeri <fahrulwalid08@gmail.com>
2026-09-09 00:33:17 +0700

    refactor(configurator): add configurator component barrel

---
+++ src/components/configurator/index.ts
@@ -0,0 +1,6 @@
+export { ConfigEditor } from './ConfigEditor';
+export type { ConfigEditorProps } from './ConfigEditor';
+export { ConfigSidebar } from './ConfigSidebar';
+export type { ConfigSidebarGroup, ConfigSidebarProps } from './ConfigSidebar';
+export { ConfigToolbar } from './ConfigToolbar';
+export type { ConfigToolbarProps } from './ConfigToolbar';
