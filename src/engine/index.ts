739b6ec24c450944b7b3c0297620445c5d237d98
Fahrul W Djuhaeri <fahrulwalid08@gmail.com>
2026-09-09 03:53:50 +0700

    feat(engine): expose config generator APIs

---
+++ src/engine/index.ts
@@ -0,0 +1,5 @@
+export { generateBusinessConfig } from './config-generator';
+export type { BusinessConfigInput } from './config-generator';
+export { normalizeBusinessInput, isValidIndustryPresetId, isValidTemplateId } from './config-schema';
+export { WebsiteSections } from './WebsiteSections';
+export { sectionRegistry, defaultSectionOrder, resolveSectionOrder, isSectionEnabled, renderSections } from './section-registry';
